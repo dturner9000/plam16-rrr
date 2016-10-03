a25d9637810d9034431303f485b2e4295becbd90
David Turner <dturner@uber.com>
2016-10-03 07:52:34 +0000

    Updated with example that would be more easily spotted by the class.

--- p3lesson1.sh
+++ p3lesson1.sh
@@ -14,7 +14,7 @@ LESSON="$0"
 stmt="delete from mysql_query_rules where comment like '%lesson%'; LOAD MYSQL QUERY RULES TO RUNTIME"
 mysql -vvv -u ${user} -p${password} -h 127.0.0.1  -P6032  -e "${stmt}"
 
-stmt="INSERT INTO mysql_query_rules (active, match_pattern, replace_pattern, comment) VALUES (1, '.*pagiwhat.*$', 'SELECT sleep(5)', \"${LESSON}\"); LOAD MYSQL QUERY RULES TO RUNTIME"
+stmt="INSERT INTO mysql_query_rules (active, match_pattern, replace_pattern, comment) VALUES (1, '.*pagiwhat.*', 'SELECT /* Query rewritten */ sleep(5)', \"${LESSON}\"); LOAD MYSQL QUERY RULES TO RUNTIME"
 mysql -vvv -u ${user} -p${password} -h 127.0.0.1  -P6032  -e "${stmt}"
 echo
 echo
